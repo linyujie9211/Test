64e2af697e4458942d095db5dc8e2ed3383515e6
linyujie9211 <linyujie9211@163.com>
2021-10-25 21:39:57 +0800

    添加test2文件

---
+++ ConsoleApp1/ConsoleApp1/Test2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Test2
+    {
+        public int MyProperty { get; set; }
+    }
+}
